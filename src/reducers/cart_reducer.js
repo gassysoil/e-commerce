59e124e93ec9cbd67210e2005102012c5fec1672
gassysoil <zhangyanjohn@gmail.com>
2023-06-23 09:59:51 -0700

    till 482

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -63,6 +63,28 @@ const cart_reducer = (state, action) => {
     });
     return { ...state, cart: tempCart };
   }
+  // if (action.type === COUNT_CART_TOTALS) {
+  //   const { total_items, total_amount } = state.cart.reduce(
+  //     (total, cartItem) => {
+  //       const { amount, price } = cartItem;
+  //       total.total_items += amount;
+  //       total.total_amount += price * amount;
+  //       return total;
+  //     },
+  //     { total_items: 0, total_amount: 0 }
+  //   );
+  //   return { ...state, total_items, total_amount };
+  // }
+
+  if (action.type === COUNT_CART_TOTALS) {
+    let total_items = 0;
+    let total_amount = 0;
+    state.cart.forEach((item) => {
+      total_amount += item.price * item.amount;
+      total_items += item.amount;
+    });
+    return { ...state, total_items, total_amount };
+  }
 
   throw new Error(`No Matching "${action.type}" - action type`);
 };
